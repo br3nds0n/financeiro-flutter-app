53aafa9a2acbda00858908b789d049ffc646a930
Brendson Silva <brendson.net@gmail.com>
2023-10-22 15:36:07 -0300

    feat: atualizando onboarding page

--- lib/common/widgets/primary_button.dart
+++ lib/common/widgets/primary_button.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../constans/app_colors.dart';
 import '../constans/app_text_styles.dart';
 
-
 class PrimaryButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final String text;
@@ -14,35 +13,32 @@ class PrimaryButton extends StatelessWidget {
     required this.text,
   }) : super(key: key);
 
+  final BorderRadius _borderRadius = const BorderRadius.all(
+    Radius.circular(24.0),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      borderRadius: const BorderRadius.all(Radius.circular(38.0)),
-      child: Ink(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(38.0)),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: onPressed != null
-                ? AppColors.greenGradient
-                : AppColors.greyGradient,
-          ),
+    return Ink(
+      height: 48.0,
+      decoration: BoxDecoration(
+        borderRadius: _borderRadius,
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: onPressed != null
+              ? AppColors.greenGradient
+              : AppColors.greyGradient,
         ),
-        child: InkWell(
-          borderRadius: const BorderRadius.all(Radius.circular(38.0)),
-          onTap: onPressed,
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(38.0)),
-            ),
-            alignment: Alignment.center,
-            height: 64.0,
-            child: Text(
-              text,
-              style: AppTextStyles.mediumText18.copyWith(
-                color: AppColors.white,
-              ),
+      ),
+      child: InkWell(
+        borderRadius: _borderRadius,
+        onTap: onPressed,
+        child: Align(
+          child: Text(
+            text,
+            style: AppTextStyles.mediumText18.copyWith(
+              color: AppColors.white,
             ),
           ),
         ),
--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -1,7 +1,11 @@
+
+import 'dart:developer';
+
 import 'package:financeiro_app/common/constans/app_colors.dart';
 import 'package:financeiro_app/common/constans/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../common/widgets/multi_text_button.dart';
 import '../../common/widgets/primary_button.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -16,9 +20,8 @@ class OnboardingPage extends StatelessWidget {
           children: [
             const SizedBox(height: 48.0),
             Expanded(
-              flex: 2,
               child: Image.asset(
-                'assets/images/man.png',
+                'assets/images/onboarding_image.png',
               ),
             ),
             Text(
@@ -34,21 +37,33 @@ class OnboardingPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
-                vertical: 16.0,
+              padding: const EdgeInsets.only(
+                left: 32.0,
+                right: 32.0,
+                top: 16.0,
+                bottom: 4.0,
               ),
               child: PrimaryButton(
                 text: 'Get Started',
                 onPressed: () {},
               ),
             ),
-            Text(
-              'Already have account? Log In',
-              style: AppTextStyles.smallText.copyWith(
-                color: AppColors.grey,
+            MultiTextButton(
+              onPressed: () => log('Log In'),
+              children: [
+              Text(
+                'Already have account? ',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.grey,
+                ),
               ),
-            ),
+              Text(
+                'Log In',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.greenTwo,
+                ),
+              ),
+            ]),
             const SizedBox(height: 24.0),
           ],
         ),
