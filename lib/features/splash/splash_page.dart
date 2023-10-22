baf97d7a03fade66f922355ddaa476f1534cb96e
Brendson Silva <brendson.silva@equipeblue.com.br>
2023-10-21 23:43:49 -0300

    feat: criando splash page

---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:financeiro_app/common/constans/app_colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../common/constans/app_text_styles.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: AppColors.greenGradient,
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Text(
+            'Financeiro App',
+            style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+          )),
+    );
+  }
+}
